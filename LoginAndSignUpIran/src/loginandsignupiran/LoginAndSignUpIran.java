a339bcbe19d011c16dbb5a0aab94627dfa4ccdfb
k2john <johnmenguis@gmail.com>
2024-12-13 03:14:55 +0800

    Add files via upload

---
+++ LoginAndSignUpIran/src/loginandsignupiran/LoginAndSignUpIran.java
@@ -0,0 +1,15 @@
+ 
+package loginandsignupiran;
+
+ 
+public class LoginAndSignUpIran {
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
